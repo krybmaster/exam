bb23cb14f866e5146594d8c1d047ddd3f6cd1bff
unknown <Konstantine>
2018-12-04 18:24:22 +0300

    Улучшил форму авторизации

--- src/components/Authorize.vue
+++ src/components/Authorize.vue
@@ -2,35 +2,47 @@
 
 <v-container grid-list-xl text-xs-left>
   <v-layout row wrap>
-     <v-flex xs12 sm5 md5 offset-xs0 >
+    <v-flex xs12 sm5 md5 offset-xs0  >
+      <v-card class="elevation-12">
+        <v-toolbar dark color="primary">
+          <v-toolbar-title>Вход пользователя</v-toolbar-title>
+        </v-toolbar>
+        <v-card-text> 
+        <form >
+          <v-text-field
+            v-model="name"
+            :error-messages="nameErrors"
+            :counter="40"
+            label="Name"
+            required
+            @input="$v.name.$touch()"
+            @blur="$v.name.$touch()"
+            prepend-icon="person"
+          ></v-text-field>
+          <v-text-field
+            v-model="email"
+            :error-messages="emailErrors"
+            label="E-mail"
+            required
+            @input="$v.email.$touch()"
+            @blur="$v.email.$touch()"
+            prepend-icon="email"
+          ></v-text-field>
+        </form>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn @click="submit">submit</v-btn>
+          <v-btn @click="clear">clear</v-btn>
+        </v-card-actions>
+      </v-card>
 
-      <v-card-text> Вход пользователя </v-card-text>
-
-      <form>
-        <v-text-field
-          v-model="name"
-          :error-messages="nameErrors"
-          :counter="40"
-          label="Name"
-          required
-          @input="$v.name.$touch()"
-          @blur="$v.name.$touch()"
-        ></v-text-field>
-        <v-text-field
-          v-model="email"
-          :error-messages="emailErrors"
-          label="E-mail"
-          required
-          @input="$v.email.$touch()"
-          @blur="$v.email.$touch()"
-        ></v-text-field>
-        <v-btn @click="submit">submit</v-btn>
-        <v-btn @click="clear">clear</v-btn>
-      </form>
     </v-flex>
   </v-layout>
 
   </v-container>
+
+  
 
 
 </template>
