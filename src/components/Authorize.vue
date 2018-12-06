9cb2c3fba12a919d1de979190f2f8b7e59627c33
unknown <Konstantine>
2018-12-06 16:31:36 +0300

    bugfix

--- src/components/Authorize.vue
+++ src/components/Authorize.vue
@@ -1,46 +1,45 @@
 <template>
-
-<v-container grid-list-xl text-xs-left>
-  <v-layout row wrap>
-    <v-flex xs12 sm5 md5 offset-xs0  >
-      <v-card class="elevation-12">
-        <v-toolbar dark color="primary">
-          <v-toolbar-title>Вход пользователя</v-toolbar-title>
-        </v-toolbar>
-        <v-card-text> 
-        <form >
-          <v-text-field
-            v-model="name"
-            :error-messages="nameErrors"
-            :counter="40"
-            label="Name"
-            required
-            @input="$v.name.$touch()"
-            @blur="$v.name.$touch()"
-            prepend-icon="person"
-          ></v-text-field>
-          <v-text-field
-            v-model="email"
-            :error-messages="emailErrors"
-            label="E-mail"
-            required
-            @input="$v.email.$touch()"
-            @blur="$v.email.$touch()"
-            prepend-icon="email"
-          ></v-text-field>
-        </form>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn @click="submit">submit</v-btn>
-          <v-btn @click="clear">clear</v-btn>
-        </v-card-actions>
-      </v-card>
-
-    </v-flex>
-  </v-layout>
-
+<v-app id="authorize">
+  <v-container grid-list-xl text-xs-left>
+    <v-layout row wrap>
+      <v-flex xs12 sm5 md5 offset-xs0  >
+        <v-card class="elevation-12">
+          <v-toolbar dark color="primary">
+            <v-toolbar-title>Вход пользователя</v-toolbar-title>
+          </v-toolbar>
+          <v-card-text> 
+          <form >
+            <v-text-field
+              v-model="name"
+              :error-messages="nameErrors"
+              :counter="40"
+              label="Name"
+              required
+              @input="$v.name.$touch()"
+              @blur="$v.name.$touch()"
+              prepend-icon="person"
+            ></v-text-field>
+            <v-text-field
+              v-model="email"
+              :error-messages="emailErrors"
+              label="E-mail"
+              required
+              @input="$v.email.$touch()"
+              @blur="$v.email.$touch()"
+              prepend-icon="email"
+            ></v-text-field>
+          </form>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn @click="submit">submit</v-btn>
+            <v-btn @click="clear">clear</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
+    </v-layout>
   </v-container>
+</v-app>
 
   
 
