fb1d456c408d1e3580e9fe41ba43e627480a1980
unknown <Konstantine>
2018-12-06 13:56:45 +0300

    !!!

--- src/components/User.vue
+++ src/components/User.vue
@@ -6,54 +6,31 @@
       <v-layout >
         <v-flex xs12 sm5 md5 offset-xs0>
 
-      <v-toolbar dark color="primary">
-        <v-toolbar-title>Темы</v-toolbar-title>
-      </v-toolbar>
-      <v-card>
+          <v-toolbar dark color="primary">
+            <v-toolbar-title>Темы</v-toolbar-title>
+          </v-toolbar>
+          <v-card>
 
-        <v-card-text>
-
-          <div> {{selectTheme}} </div> 
-              
-
-              <v-radio-group v-model="radioTheme">
-                
+            <v-card-text>
+              <div> {{selectTheme}} </div> 
+              <v-radio-group v-model="radioTheme">  
                 <v-radio
                   v-for="theme in themes" :key="theme.id"
                   :label="`${theme.text}`"
                   :value="theme.id">
                 </v-radio>
               </v-radio-group>
-              <v-divider> </v-divider>
-
-
-          <!--
-         
-          <v-card class="mb-4" v-for="theme in themes" :key="theme.id">
-            <v-card-title primary-title>
-              
-              {{ theme.text }}
-               
-            </v-card-title>
+              <v-divider> </v-divider>     
+            </v-card-text>
             <v-card-actions>
-              <v-btn flat color="orange" @click="startTheme(theme.id)">Выбрать</v-btn>
+              <v-spacer> </v-spacer>
+              <v-btn :disabled="radioTheme === null" justify-end color="primary" @click="startTheme()">Далее</v-btn>
             </v-card-actions>
+
           </v-card>
 
-  -->
-          
-         
-      
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer> </v-spacer>
-          <v-btn :disabled="radioTheme === null" justify-end color="primary" @click="startTheme()">Далее</v-btn>
-        </v-card-actions>
-
-      </v-card>
-
         </v-flex>
-        </v-layout>
+      </v-layout>
 
     </v-container>
 
